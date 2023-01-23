96c8bb8e961d8ebbec482e574aae41b660d59eb3
valentinacamacho <valentinacamacho0206@gmail.com>
2023-01-23 11:40:49 -0500

    comments in List and Section components js

--- src/components/List.js
+++ src/components/List.js
@@ -1,19 +1,26 @@
 import React from "react";
 import Todo from "./Todo";
 
+/*certain properties are created */
 const List = ({ list, removeTodoListProp, editTodoListProp }) => {
+    /*The .map is a method so that you can iterate each list that is added */
     const renderedList = list.map(
         (item) => (
+            /*calls the TODO component where you are going to call the item variable with your object */
             <Todo
                 title={item.title}
                 completed={item.completed}
+                /*by means of this property removeTodoListProp contains an event where
+                 you can remove each item from the list by means of the id */
                 removeTodoItemProp={(e) => removeTodoListProp(item._id)}
+                /* through the editTodoListProp method can edit via id*/
                 editTodoItemProp={(updatedItem) => editTodoListProp(item._id, updatedItem)}
                 key={item.title}
             />
         )
     );
     return (
+        /*calls the class and renders the renderedList property */
         <div className="ui grid center aligned">
             {renderedList}
         </div>
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,9 @@
 import React from "react";
 
+/*The section component is defined as a simple component that simply represents a div element. */
+/*Children corresponds to the props attribute of a React element */
 const Section = ({ children }) => {
+  /*The children property is holding a div element that has a class spacing*/
   return <div style={{ margin: "50px" }}>{children}</div>;
 };
 
