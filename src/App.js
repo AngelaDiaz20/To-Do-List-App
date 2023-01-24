e69c5f462eee38735edad0c99952d28c298a0fac
Jlbejarano662 <jasmin.1207@hotmail.com>
2023-01-24 10:14:59 -0500

    stylesstyle improvement, missing background image

--- src/App.js
+++ src/App.js
@@ -6,59 +6,60 @@ import Section from "./components/Section";
 import List from "./components/List";
 import Footer from "./components/Footer";
 
-import "./App.css"
+import "./App.css";
 
-const appTitle = "To-Do App";
+const appTitle = "To-Do List";
 
 const App = () => {
-    const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState([]);
 
-    useEffect(() => {
-        async function fetchData() {
-            const { data } = await todos.get("/todos");
-            setTodoList(data);
-        }
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await todos.get("/todos");
+      setTodoList(data);
+    }
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    const addTodo = async (item) => {
-        const { data } = await todos.post("/todos", item);
-        setTodoList((oldList) => [...oldList, data]);
-    };
+  const addTodo = async (item) => {
+    const { data } = await todos.post("/todos", item);
+    setTodoList((oldList) => [...oldList, data]);
+  };
 
-    const removeTodo = async (id) => {
-        await todos.delete(`/todos/${id}`);
-        setTodoList((oldList) => oldList.filter((item) => item._id !== id));
-    };
+  const removeTodo = async (id) => {
+    await todos.delete(`/todos/${id}`);
+    setTodoList((oldList) => oldList.filter((item) => item._id !== id));
+  };
 
-    const editTodo = async (id, item) => {
-        await todos.put(`/todos/${id}`, item);
-    };
+  const editTodo = async (id, item) => {
+    await todos.put(`/todos/${id}`, item);
+  };
 
-    return (
-        <div className="content">
-            <img class="ui centered big image" src="https://www.nippon.com/es/ncommon/contents/guide-to-japan/143942/143942.jpg" width={"100%"} alt=""
-                style={{ zIndex: -10 }} />
-                <Section>
-                    <h1>{appTitle}</h1>
-                </Section>
+  return (
+    <div className="content">
+      <div style={{width: "100%"}}>
+        {/* <img class="ui centered big image" src="https://www.nippon.com/es/ncommon/contents/guide-to-japan/143942/143942.jpg" width={"100%"} alt=""
+                style={{ zIndex: -10 }} /> */}
+        <Section>
+          <h1>{appTitle}</h1>
+        </Section>
 
-                <Section>
-                    <Form addTodo={addTodo} />
-                </Section>
+        <Section>
+          <Form addTodo={addTodo} />
+        </Section>
 
-                <Section>
-                    <List
-                        editTodoListProp={editTodo}
-                        removeTodoListProp={removeTodo}
-                        list={todoList}
-                    />
-                </Section>
-                <Footer/>
-            </div>
-
-    );
+        <Section>
+          <List
+            editTodoListProp={editTodo}
+            removeTodoListProp={removeTodo}
+            list={todoList}
+          />
+        </Section>
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default App;
