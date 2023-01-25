fbb57d543e8e55a0e026f988f837b3d2ac183db4
Jlbejarano662 <jasmin.1207@hotmail.com>
2023-01-25 15:32:23 -0500

    valentina

--- src/components/List.js
+++ src/components/List.js
@@ -15,13 +15,29 @@ const List = ({ list, removeTodoListProp, editTodoListProp }) => {
       key={item.title}
     />
   ));
+
+  const showCompleted = () => {
+    // hide pending tasks
+    const pendingTasks = document.querySelectorAll(".false");
+    pendingTasks.forEach((task) => {
+      task.style.display = "none";
+    });
+    document.querySelector(".true.divider").style.display = "none";
+
+    /* //show completed tasks
+    const completedTasks = document.querySelectorAll(".true");
+    completedTasks.forEach((task) => {
+      task.style.display = "block";
+    }); */
+  };
+
   return (
     <div className="list">
       <div className="ui grid center aligned tasks">{renderedList}</div>
       <div className="filters">
         <a>Todas</a>
         <a>Pendientes</a>
-        <a>Completadas</a>
+        <a onClick={showCompleted}>Completadas</a>
       </div>
     </div>
   );
