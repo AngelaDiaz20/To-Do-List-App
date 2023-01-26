a97c5eeed0a7818895dc41aa888db99921101432
Angela Diaz <angeldi846@gmail.com>
2023-01-26 09:50:16 -0500

    connection with database

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3030"
+    // baseURL: "http://localhost:3030"
+    baseURL: "https://flores-del-tambo-to-do-list-bd.vercel.app/"
 });
